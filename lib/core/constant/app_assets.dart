286ecffd4aac7bb9b7d314f12c3ea45546813eee
dejito <Deerealboy@gmail.com>
2025-06-02 18:11:54 +0100

    implemented app architecture/folder structuring

---
+++ lib/core/constant/app_assets.dart
@@ -0,0 +1,14 @@
+
+
+class AppImages {
+  static const brandLogo = 'assets/drawables/brand_logo.webp';
+  static const onboardingOne = 'assets/drawables/onboarding_one.webp';
+  static const onboardingTwo = 'assets/drawables/onboarding_two.jpg';
+  static const onboardingThree = 'assets/drawables/onboarding_three.webp';
+  static const logo2 = 'assets/drawables/logo2.webp';
+
+}
+
+class AppIcons {
+
+}
