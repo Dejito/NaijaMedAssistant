4e859fa2e0263ee64c111c58ca67e41b9ee5ee32
dejito <Deerealboy@gmail.com>
2025-06-02 18:14:06 +0100

    login_screen.dart ui implementation started

--- lib/presentation/views/auth/login/login_screen.dart
+++ lib/presentation/views/auth/login/login_screen.dart
@@ -7,7 +7,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        padding: ,
+        padding: EdgeInsets.all(16.w),
         child: Column(
           children: [
 
