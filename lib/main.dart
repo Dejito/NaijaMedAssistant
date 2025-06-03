4a5fb46c2878f0921897f884f18b6d001043eeba
dejito <Deerealboy@gmail.com>
2025-06-03 09:49:45 +0100

    gorouting set up on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:naija_med_assistant/presentation/views/auth/login/login_screen.dart';
+import 'package:naija_med_assistant/router/route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +16,14 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       minTextAdapt: true,
       designSize: const Size(390, 884),
-      child: MaterialApp(
+      child: MaterialApp.router(
+        routerConfig: router,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const LoginScreen(),
+        // home: const LoginScreen(),
       ),
     );
   }
